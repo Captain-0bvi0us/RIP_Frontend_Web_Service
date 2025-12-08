97075401caec483f99566fef0043a93baefa3a01
Alex Last <montevidio@inbox.ru>
2025-12-08 17:03:58 +0300

    + .env

--- frax-frontend/dev-dist/sw.js
+++ frax-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g6rsihg8tdo"
+    "revision": "0.cvii4ji1hpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
